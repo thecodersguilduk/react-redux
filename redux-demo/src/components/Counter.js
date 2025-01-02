8f11711dcf696b7b94925b1c2e4de98a1d2e0bc5
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2025-01-02 15:32:29 +0000

    add react and simple counter component

---
+++ redux-demo/src/components/Counter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Counter = () => {
+
+  return (
+    <div>
+      <h1>Counter: </h1>
+      <button>Increment</button>
+      <button>Decrement</button>
+      <button>Reset</button>
+    </div>
+  );
+};
+
+export default Counter;
